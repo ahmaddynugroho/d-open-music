8420250ee6fff641ceecf4525aa10b2a969b6bd7
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-23 20:40:08 +0700

    feat: hapi hello world

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
 import "dotenv/config";
-import "./cunny.ts";
+import Hapi from "@hapi/hapi";
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: () => {
+      return "Hello World!";
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
