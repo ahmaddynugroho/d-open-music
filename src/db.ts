1632a9997c677c2f1387c3fb52b42b0552e5de5c
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-27 17:42:27 +0700

    db: extract album

--- src/db.ts
+++ src/db.ts
@@ -1,65 +1,11 @@
 import { nanoid } from "nanoid";
 import pg from "pg";
 import bcrypt from "bcrypt";
-import { Album } from "./schemas/album.ts";
 import { Song } from "./schemas/song.ts";
 import { User } from "./schemas/user.ts";
 const { Pool } = pg;
 
-const pool = new Pool();
-
-export const addAlbum = async (name: string, year: number) => {
-  const id = nanoid();
-  const client = await pool.connect();
-  await client.query(
-    `
-      INSERT INTO albums (id, name, year)
-      VALUES ($1, $2, $3)
-    `,
-    [id, name, year],
-  );
-  await client.release();
-  return id;
-};
-
-export const getAlbum = async (id: string) => {
-  const client = await pool.connect();
-  const album: pg.QueryResult<Album> = await client.query(
-    `
-      SELECT *
-      FROM albums
-      WHERE id=$1
-    `,
-    [id],
-  );
-  await client.release();
-  return album;
-};
-
-export const putAlbum = async (id: string, name: string, year: number) => {
-  const client = await pool.connect();
-  await client.query(
-    `
-      UPDATE albums
-      SET name=$1, year=$2
-      WHERE id=$3
-    `,
-    [name, year, id],
-  );
-  await client.release();
-};
-
-export const deleteAlbum = async (id: string) => {
-  const client = await pool.connect();
-  await client.query(
-    `
-      DELETE FROM albums
-      WHERE id=$1
-    `,
-    [id],
-  );
-  await client.release();
-};
+export const pool = new Pool();
 
 export const addSong = async (songBody: Song) => {
   const { title, year, performer, genre, duration, albumId } = songBody;
