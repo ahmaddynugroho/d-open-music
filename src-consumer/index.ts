1a69bca841e634d349be9cb9f48061e0f5545ca9
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-09-28 17:38:42 +0700

    add export-mail placeholder

---
+++ src-consumer/index.ts
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import amqp from "amqplib/callback_api.js";
+import nodemailer from "nodemailer";
+
+// Looking to send emails in production? Check out our Email API/SMTP product!
+const transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+const send = (targetEmail, content) => {
+  const message = {
+    from: "Notes Apps",
+    to: targetEmail,
+    subject: "Ekspor Catatan",
+    text: "Terlampir hasil dari ekspor catatan",
+    attachments: [
+      {
+        filename: "notes.json",
+        content,
+      },
+    ],
+  };
+  return transport.sendMail(message);
+};
+
+amqp.connect(process.env.RABBITMQ_SERVER, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    const queue = "export:playlist";
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+    channel.consume(
+      queue,
+      async (msg) => {
+        const res = await send("me@me.me", msg.content.toString());
+        console.log(res);
+
+        // transport.sendMail(msg.content.toString(), () => {
+        //   console.log(" [x] Received %s", msg.content.toString());
+        // });
+      },
+      {
+        noAck: true,
+      },
+    );
+  });
+});
